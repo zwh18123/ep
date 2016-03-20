1e6f4acaf7a29c2fd419b15350b13cd1c3b8ba4b
zwh <zwh18123@sina.com>
2016-03-20 22:45:11 +0800

    3

--- b.py
+++ b.py
@@ -1,14 +1,15 @@
 #12322222222222222222
 import qrcode
 import Image
-id= raw_input("Input your id plz")
-id = '44170' + id
-print(id)
-#fsdfasdwhile 1:
-if id != 0 :
-    img = qrcode.make( id )
-    img.show()
-else :
-    print(id)
+
+
+for i in range(1,5):
+    id= raw_input("Input your id plz")
+    if id != '0' :    
+        id = '44170' + id
+        img = qrcode.make( id )
+        img.show()
+    else :
+        break
 
 print('ok')
