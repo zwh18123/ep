d8d23fec885514653848738f490a066b05f93395
zwh <zwh@g.com>
2016-03-20 21:07:43 +0800

    init 0
    ok

---
+++ b.py
@@ -0,0 +1,13 @@
+import qrcode
+import Image
+id= raw_input("Input your id plz")
+id = '44170' + id
+print(id)
+#fsdfasdwhile 1:
+if id != 0 :
+    img = qrcode.make( id )
+    img.show()
+else :
+    print(id)
+
+print('ok')
